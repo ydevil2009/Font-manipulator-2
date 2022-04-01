7bc2f270ec839596dc41f1ca3bf4d5618bc324f8
ydevil2009 <85282146+ydevil2009@users.noreply.github.com>
2022-04-01 13:49:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+
+difference = 0;
+rightWristX = 0;
+leftWristX = 0
+function setup() {
+    video = createCapture(VIDEO); 
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+} 
+
+
+function modelLoaded() {
+    console.log('poseNet Is Innitialized! ');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+      console.log(results);
+      leftWristX = results[0].pose.leftWrist.x;
+      rightWristX = results[0].pose.rightWrist.x;
+      difference = floor(leftWristX - rightWristX);
+  
+      console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+    }
+  }
+
+  function draw() {
+    background('#969A97');
+    document.getElementById("font_size").innerHTML = "Width And Height of a Font will be = " + difference + "px";
+    textSize(difference);
+    fill('#FFE787');
+    text('YASH', 50, 400);
+}
